4d54f0ce1a66a39e88b06c431be8316c85f12a6c
Kulltero <33698270+Kulltero@users.noreply.github.com>
2022-10-24 17:40:34 +0200

    Caching, optimization when alpha = 0
    - changed from Animation from MonoBehaviour to FacepunchBehaviour
    - Caching the Graphic components on Start(), should be better over constantly calling GetComponents, 
      - this meains that Start will have to be called after all components are added (follow up commit comming)
    - Added Reference to Animation to each property on start, this is required because properties didnt have a reference to the gameObject so they couldnt get the graphics to change its properties prior to caching
    - Allowed the ability to Toggle Transparent Mesh Culling => likely a performance improvement if there are alot of elements with Alpha 0
    - Color Animations will be instant/skipped if IsHidden & the alpha remains at 0 (or if its an Alphaless Transition)

--- Community.UI.Animation.cs
+++ Community.UI.Animation.cs
@@ -9,16 +9,22 @@ using System.IO;
 
 public partial class CommunityEntity
 {
-	private class Animation : MonoBehaviour
+	private class Animation : FacepunchBehaviour
 	{
 		public List<AnimationProperty> properties = new List<AnimationProperty>();
+		
+		public List<UnityEngine.UI.Graphic> cachedGraphics = new List<UnityEngine.UI.Graphic>();
+		
+		public bool isHidden = false;
 		
 		public bool isKilled = false;
 		
 		public void Start()
 		{
+			CacheGraphics();
 			foreach(var prop in properties)
 			{
+				prop.anim = this;
 				prop.routine = StartCoroutine(prop.Animate());
 			}
 		}
@@ -35,6 +41,23 @@ public partial class CommunityEntity
 		private void OnDestroy(){
 			if(!isKilled) Kill();
 		}
+		
+		public void CacheGraphics(){
+			cachedGraphics.Clear();
+			gameObject.GetComponents<UnityEngine.UI.Graphic>(cachedGraphics);
+		}
+		public void ToggleTCulling(bool b){
+			foreach(var c in cachedGraphics){
+				c.cullTransparentMesh = b;
+			}
+		}
+		public void WaitToggleHidden(float delay = 0f){
+			if(delay <= 0f) ToggleTCulling(true);
+			else Invoke(new Action(() => {
+				ToggleTCulling(true);
+				isHidden = true;
+			}), delay);
+		}
 	}
 	
 	private class AnimationProperty
@@ -46,6 +69,8 @@ public partial class CommunityEntity
 		public string type;
 		public string from;
 		public string to;
+		
+		public Animation anim;
 		
 		public Coroutine routine;
 		
@@ -79,11 +104,16 @@ public partial class CommunityEntity
 						if(!float.TryParse(to, out toOpacity)) break;
 						// unlike the toOpacity, a from value is optional. if we omit a from value the crossfade will instead use the current alpha
 						float.TryParse(from, out fromOpacity);
-						
-						foreach(var c in gameObject.GetComponents<UnityEngine.UI.Graphic>()){
+						// disable TCulling if the toOpacity or FromOpacity are Higher than 0f, if the fromOpacity check was ommited this wouldnt trigger if its an animation from > 0 to 0
+						if(anim.isHidden && toOpacity > 0f || (fromOpacity != null && fromOpacity > 0f)){
+							anim.ToggleTCulling(false);
+							anim.isHidden = false;
+						}
+						foreach(var c in anim.cachedGraphics){
 							if(fromOpacity != null) c.canvasRenderer.SetAlpha(fromOpacity);
 							c.CrossFadeAlpha(toOpacity, duration, true);
 						}
+						if(!anim.isHidden && toOpacity <= 0f) anim.WaitToggleHidden(duration);
 						
 						break;
 					}
@@ -95,9 +125,9 @@ public partial class CommunityEntity
 						//dont use the from color if the from value was ommited
 						bool useFrom = string.IsNullOrEmpty(from);
 						
-						foreach(var c in gameObject.GetComponents<UnityEngine.UI.Graphic>()){
-							if(useFrom) c.canvasRenderer.SetColor(fromColor);
-							c.CrossFadeColor(toColor, duration, true, false);
+						foreach(var c in anim.cachedGraphics){
+							if(useFrom && !anim.isHidden) c.canvasRenderer.SetColor(fromColor);
+							c.CrossFadeColor(toColor, (anim.isHidden ? 0f : duration), true, false);
 						}
 						
 						break;
@@ -109,11 +139,17 @@ public partial class CommunityEntity
 						
 						//dont use the from color if the from value was ommited
 						bool useFrom = string.IsNullOrEmpty(from);
-						
-						foreach(var c in gameObject.GetComponents<UnityEngine.UI.Graphic>()){
-							if(useFrom) c.canvasRenderer.SetColor(fromColor);
-							c.CrossFadeColor(toColor, duration, true, true);
+						if(anim.isHidden && toColor.a > 0f || (useFrom && fromColor.a > 0f)){
+							anim.ToggleTCulling(false);
+							anim.isHidden = false;
 						}
+						bool doInstant = toColor.a <= 0f && (useFrom && fromColor.a <= 0f) || anim.isHidden;
+						foreach(var c in anim.cachedGraphics){
+							if(useFrom && !doInstant) c.canvasRenderer.SetColor(fromColor);
+							c.CrossFadeColor(toColor, (doInstant ? 0f : duration), true, true);
+						}
+						
+						if(!anim.isHidden && toColor.a <= 0f) anim.WaitToggleHidden(duration);
 						
 						break;
 					}
