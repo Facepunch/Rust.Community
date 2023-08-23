f7606ae0681247a37ba3e8655a5f810f554efed9
Kulltero <33698270+Kulltero@users.noreply.github.com>
2023-08-23 05:52:42 +0200

    Simplified Bezier Easing Implementation
    overall cleanup, naming improvements & only things that need to be public are public
    
    the math is still undocumented magic, but thats what math just is
    
    preperation for BezierPoints to be used directly, defined some default curves for comparison use

--- CommunityEntity.UI.BezierEasing.cs
+++ CommunityEntity.UI.BezierEasing.cs
@@ -11,146 +11,188 @@ using System.IO;
 public partial class CommunityEntity
 {
 
-    public class BezierEasing {
-        // this basically is the same process that Browsers use for their CSS easing, allowing you to define any possible easing value with 4 bezier points
-        // NOTE: this is a C# version of the javascript library at https://github.com/gre/bezier-easing, i simply rewrote it to be in C# Syntax and added a Static helper method
+    public class BezierEasing
+    {
+        // the same process browsers use for the cubic-bezier css timing function
+        // from the spec:
+        //     "A cubic Bézier easing function is a type of easing function defined by four real numbers that specify the two control points, P1 and P2,
+        //      of a cubic Bézier curve whose end points P0 and P3 are fixed at (0, 0) and (1, 1) respectively.
+        //      The x coordinates of P1 and P2 are restricted to the range [0, 1]."
+        // NOTE: Loosely based on https://github.com/gre/bezier-easing
 
-        public static Dictionary<BezierPoints, BezierEasing> cache = new Dictionary<BezierPoints, BezierEasing>();
+        static Dictionary<BezierPoints, BezierEasing> cache = new Dictionary<BezierPoints, BezierEasing>();
 
         // these values modify the accuracy & performance of the curve's evaluation
         // while these are the values used in the javascript library you may find better values that produce good enough results with better performance
-        public static int NewItts = 4;
-        public static float NewMinSlope = 0.001f;
-        public static float SubDivPrec = 0.0000001f;
-        public static float SubDivItts = 10;
+        static int NewtonItterations = 4;
 
-        public float mX1 = 0f;
-        public float mY1 = 0f;
-        public float mX2 = 1f;
-        public float mY2 = 1f;
-        public float[] mSampleValues;
+        public float firstX = 0f;
+        public float firstY = 0f;
+        public float secondX = 1f;
+        public float secondY = 1f;
+        float[] _precomputedSections;
 
-        public static int kSplineTableSize = 11;
-        public static float kSampleStepSize = 1.0f / (kSplineTableSize - 1.0f);
+        static int _precomputeSize = 11;
+        static float _precomputeStepSize = 1.0f / (_precomputeSize - 1.0f);
         bool _precomputed = false;
 
 
+        // its beneficial to call this when we want to reset our session, like when leaving/switching servers
+        public static void Clear()
+        {
+            cache.Clear();
+        }
+
         // Helper function that caches BezierEasing Instances. supplying 4 points & time between 0 & 1 generates the curve & caches it, returning the value for time
-        public static float Ease(float X1, float Y1, float X2, float Y2, float time){
+        public static float Ease(float X1, float Y1, float X2, float Y2, float time)
+        {
             var key = new BezierPoints(X1, Y1, X2, Y2);
-            if(!cache.ContainsKey(key)){
-                cache[key] = new BezierEasing(X1, Y1, X2, Y2);
+            return Ease(key, time);
+        }
+
+
+        public static float Ease(BezierPoints points, float time)
+        {
+            if (!cache.TryGetValue(points, out BezierEasing easing))
+                cache[points] = easing = new BezierEasing(points);
+
+            return easing.GetPosition(time);
+        }
+
+        public BezierEasing(BezierPoints points) : this(points.P1, points.P2, points.P3, points.P4)
+        {
+
+        }
+
+        public BezierEasing(float X1, float Y1, float X2, float Y2)
+        {
+            firstX = X1;
+            firstY = Y1;
+            secondX = X2;
+            secondY = Y2;
+            _precomputedSections = new float[_precomputeSize];
+        }
+
+
+
+        // returns the value on the curve for X
+        public float GetPosition(float unscaledTime)
+        {
+            if (!_precomputed) PreCompute();
+            if (firstX == firstY && secondX == secondY) return unscaledTime; // linear
+            if (unscaledTime == 0f) return 0f;
+            if (unscaledTime == 1f) return 1f;
+            return CalcBezier(ScaleTime(unscaledTime), firstY, secondY);
+        }
+
+        // the following 2 functions are part of a single math formula split up into 2 steps for clarity
+        float StepA(float aA1, float aA2) { return 1.0f - 3.0f * aA2 + 3.0f * aA1; }
+        float StepB(float aA1, float aA2) { return 3.0f * aA2 - 6.0f * aA1; }
+
+        // Returns calculates the value between 2 points based on call
+        // points may be x or y depending on call
+        float CalcBezier(float time, float point1, float point2)
+        {
+            return ((StepA(point1, point2) * time + StepB(point1, point2)) * time + (3f * point1)) * time;
+        }
+
+        // Returns the estimated slope of the curve at the supplied time
+        float GetSlope(float time)
+        {
+            return 3.0f * StepA(firstX, secondX) * time * time + 2.0f * StepB(firstX, secondX) * time + (3f * firstX);
+        }
+
+        // pre-calculates a set amount of points to spped up calculation
+        void CalcSampleValues()
+        {
+            for (var i = 0; i < _precomputeSize; ++i)
+            {
+                _precomputedSections[i] = CalcBezier(i * _precomputeStepSize, firstX, secondX);
             }
-            return cache[key].GetPosition(time);
         }
 
-        public BezierEasing (float X1, float Y1, float X2, float Y2) {
-            mX1 = X1;
-            mY1 = Y1;
-            mX2 = X2;
-            mY2 = Y2;
-            mSampleValues = new float[kSplineTableSize];
-        }
-
-
-        // the following 3 functions are a single math formula split up into 3 steps for clarity
-        float A (float aA1, float aA2) { return 1.0f - 3.0f * aA2 + 3.0f * aA1; }
-        float B (float aA1, float aA2) { return 3.0f * aA2 - 6.0f * aA1; }
-        float C (float aA1) { return 3.0f * aA1; }
-
-        // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
-        public float CalcBezier (float aT, float aA1, float aA2) {
-            return ((A(aA1, aA2)*aT + B(aA1, aA2))*aT + C(aA1))*aT;
-        }
-
-        // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
-        public float GetSlope (float aT, float aA1, float aA2) {
-            return 3.0f * A(aA1, aA2)*aT*aT + 2.0f * B(aA1, aA2) * aT + C(aA1);
-        }
-
-        // performs a dichometric search to find the most accurate value between 2 precalculated points within SubDivItts loops
-        public float BinarySubdivide (float aX, float aA, float aB) {
-            float currentX, currentT, i = 0f;
-            do {
-                currentT = aA + (aB - aA) / 2.0f;
-                currentX = CalcBezier(currentT, mX1, mX2) - aX;
-                if (currentX > 0.0f) {
-                    aB = currentT;
-                } else {
-                    aA = currentT;
-                }
-            } while (Math.Abs(currentX) > SubDivPrec && ++i < SubDivItts);
-            return currentT;
-        }
-
-        // pre-calculates a set amount of points to be used within the BinarySubdivide function
-        void CalcSampleValues () {
-            for (var i = 0; i < kSplineTableSize; ++i) {
-                mSampleValues[i] = CalcBezier(i * kSampleStepSize, mX1, mX2);
-            }
-        }
-
-        // finds the surrounding pre-calculated points for x and returns the closest value, uses NewtonRaphson's method for faster computation if the slope is above a set threshold, otherwhise falls back to a binary search
-        float GetTForX (float aX) {
+        // finds the surrounding pre-calculated sections around our unscaled time and refines it using NewtonRaphson's method
+        float ScaleTime(float unscaledTime)
+        {
             float intervalStart = 0.0f;
             int currentSample = 1;
-            int lastSample = kSplineTableSize - 1;
+            int lastSample = _precomputeSize - 1;
 
-            for (; currentSample != lastSample && mSampleValues[currentSample] <= aX; ++currentSample) {
-                intervalStart += kSampleStepSize;
+            // find the last precomputed section before unscaledTime
+            for (; currentSample != lastSample && _precomputedSections[currentSample] <= unscaledTime; ++currentSample)
+            {
+                intervalStart += _precomputeStepSize;
             }
             --currentSample;
 
-            float dist = (aX - mSampleValues[currentSample]) / (mSampleValues[currentSample+1] - mSampleValues[currentSample]);
-            float guessForT = intervalStart + dist * kSampleStepSize;
-            float initialSlope = GetSlope(guessForT, mX1, mX2);
-            if (initialSlope >= NewMinSlope) {
-                return NewtonRaphsonIterate(aX, guessForT);
-            } else if (initialSlope == 0.0f) {
-                return guessForT;
-            } else {
-                return BinarySubdivide(aX, intervalStart, intervalStart + kSampleStepSize);
+            // divide distance from last section by distance between last and next section
+            float distance = (unscaledTime - _precomputedSections[currentSample]) / (_precomputedSections[currentSample + 1] - _precomputedSections[currentSample]);
+            float timeGuess = intervalStart + distance * _precomputeStepSize;
+            float initialSlope = GetSlope(timeGuess);
+            if (initialSlope == 0.0f)
+            {
+                return timeGuess;
+            }
+            else
+            {
+                return NewtonRaphsonIterate(unscaledTime, timeGuess);
             }
         }
 
-        void PreCompute() {
+        void PreCompute()
+        {
             _precomputed = true;
-            if (mX1 != mY1 || mX2 != mY2)
+            if (firstX != firstY || secondX != secondY)
                 CalcSampleValues();
         }
 
-        // returns the value on the curve for X
-        public float GetPosition(float aX) {
-            if (!_precomputed) PreCompute();
-            if (mX1 == mY1 && mX2 == mY2) return aX; // linear
-            if (aX == 0f) return 0f;
-            if (aX == 1f) return 1f;
-            return CalcBezier(GetTForX(aX), mY1, mY2);
-        }
-
         // approximates the value for X using the Newton-Raphson Method
-        float NewtonRaphsonIterate (float aX, float aGuessT) {
-            for (var i = 0; i < NewItts; ++i) {
-                float currentSlope = GetSlope(aGuessT, mX1, mX2);
-                if (currentSlope == 0.0f) return aGuessT;
-                var currentX = CalcBezier(aGuessT, mX1, mX2) - aX;
-                aGuessT -= currentX / currentSlope;
+        float NewtonRaphsonIterate(float aX, float timeGuess)
+        {
+            for (var i = 0; i < NewtonItterations; ++i)
+            {
+                float currentSlope = GetSlope(timeGuess);
+                if (currentSlope == 0.0f) return timeGuess;
+                var currentX = CalcBezier(timeGuess, firstX, secondX) - aX;
+                timeGuess -= currentX / currentSlope;
             }
-            return aGuessT;
+            return timeGuess;
         }
 
         // used as a key for the cache
-        public struct BezierPoints{
+        public struct BezierPoints : IEquatable<BezierPoints>
+        {
+            public static readonly BezierPoints LINEAR = new BezierPoints(0f, 0f, 0f, 0f);
+            public static readonly BezierPoints EASE_IN = new BezierPoints(0.42f, 0f, 1f, 1f);
+            public static readonly BezierPoints EASE_OUT = new BezierPoints(0f, 0f, 0.58f, 1f);
+            public static readonly BezierPoints EASE_IN_OUT = new BezierPoints(0.42f, 0f, 0.58f, 1f);
+
             public float P1;
             public float P2;
             public float P3;
             public float P4;
 
-            public BezierPoints(float X1, float Y1, float X2, float Y2){
+            public BezierPoints(float X1, float Y1, float X2, float Y2)
+            {
                 P1 = X1;
                 P2 = Y1;
                 P3 = X2;
                 P4 = Y2;
+            }
+
+            public bool Equals(BezierPoints other)
+            {
+                return P1 == other.P1 && P2 == other.P2 && P2 == other.P2 && P3 == other.P3 && P4 == other.P4;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is BezierPoints points && Equals(points);
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(P1, P2, P3, P4);
             }
         }
     }
